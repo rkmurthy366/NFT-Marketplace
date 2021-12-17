e66213293d64731cf99dc1fc2a0ce0f419337bca
Radha Krishna <66104387+trk286@users.noreply.github.com>
2021-12-17 22:22:45 +0530

    Added files v0.1

---
+++ config.js
@@ -0,0 +1,2 @@
+export const nftaddress = "0xaf71e076094c970bC0A10A824a3d58943dd3246f";
+export const nftmarketaddress = "0x3d8F384b5DdccDf8aa215b85eb31E88967001355";
---
+++ hardhat.config.js
@@ -0,0 +1,43 @@
+/* hardhat.config.js */
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "rinkeby-rpc-url";
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL || "mumbai-rpc-url";
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "your private key";
+
+const ETHERSCAN_API_KEY =
+  process.env.ETHERSCAN_API_KEY || "Your etherscan API key";
+const POLYSCAN_API_KEY =
+  process.env.POLYSCAN_API_KEY || "Your polyscan API key";
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: MUMBAI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: POLYSCAN_API_KEY,
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
